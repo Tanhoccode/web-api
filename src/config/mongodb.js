2adaeb72a83e6710f9ddc7fd33c67c83dc178617
Tanhoccode <tanprovip156@gmail.com>
2024-05-08 14:37:04 +0700

    Connect-Data

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,16 +1,8 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import {env} from '~/config/environment'
 
-// u0PHMBIRE9QMb5LU
-// tanprovip156
-const MONGODB_URI = 'mongodb+srv://tanprovip156:u0PHMBIRE9QMb5LU@cluster0.2vzavnt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-const DATABASE_NAME = 'trello-backend'
 import { MongoClient, ServerApiVersion } from 'mongodb'
 let trelloDatabaseInstance = null
-const mongoClientInstance = new MongoClient(MONGODB_URI, {
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
   serverApi : {
     version: ServerApiVersion.v1,
     strict: true,
@@ -21,10 +13,15 @@ const mongoClientInstance = new MongoClient(MONGODB_URI, {
 export const CONNECT_DB = async() => {
   await mongoClientInstance.connect()
 
-  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
 }
 
 export const GET_DB = () => {
   if (!trelloDatabaseInstance) throw new Error('Must connect do DB first!')
   return trelloDatabaseInstance
 }
+
